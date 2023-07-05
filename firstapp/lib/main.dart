ccffca1a6a898d6395ea12a66a303d1a47496ed1
Raphael Busquet <raphaelbusquet@outlook.com>
2023-07-05 22:10:18 +0100

    any

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -10,8 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp( title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
