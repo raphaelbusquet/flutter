41cea0149e441a3eefe1c28d471ab9c8305b19d4
Raphael Busquet <raphaelbusquet@outlook.com>
2023-07-09 21:24:28 +0100

    implement...

--- firstapp/lib/pages/to_do_list.dart
+++ firstapp/lib/pages/to_do_list.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+  TodoListPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: TextField(
-          decoration: InputDecoration(
-            labelText: 'E-mail',
-            helperText: 'exemplo@gmail.com',
-            prefixText: 'R\$',
-            labelStyle: TextStyle(
-              fontSize: 20,
-              color: Colors.cyan,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'E-mail',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.cyan,
+                ),
+              ),
+              obscureText: true,
             ),
-          ),
-          obscureText: true,
+            ElevatedButton(
+              onPressed: login,
+              child: const Text('Entrar'),
+            ),
+          ],
         ),
       ),
     ));
+  }
+
+  void login() {
+    String text = emailController.text;
+    print(text);
+    emailController.clear();
   }
 }
